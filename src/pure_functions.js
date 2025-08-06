a8b7f96178a93861fdf4c2bff5171420ff5310af
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-08-06 11:35:53 +0400

    this is another commiti

--- src/pure_functions.js
+++
@@ -1,6 +0,0 @@
-const image_transition = (props) => {
-  const element = document.getElementById(props.element_id);
-  const [imageIndex, setImageIndex] = useState(0)
-  setInterval(() => transition(element, props.imageList, imageIndex, setImageIndex), 5000)
-};
-
